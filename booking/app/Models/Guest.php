08a471157c2fc0dc18b3024483ce4494de632c78
V L Y <89966395+v-l-y@users.noreply.github.com>
2026-09-25 00:20:30 +0530

    Store guest billing identity

--- booking/app/Models/Guest.php
+++ booking/app/Models/Guest.php
@@ -7,7 +7,10 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Guest extends Model
 {
-    protected $fillable = ['first_name', 'last_name', 'phone', 'email'];
+    protected $fillable = [
+        'first_name', 'last_name', 'phone', 'email',
+        'gstin', 'billing_address', 'billing_state', 'billing_state_code',
+    ];
 
     public function reservationLinks(): HasMany
     {
