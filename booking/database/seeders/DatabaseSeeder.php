300eba58cf02f0df938dcc30605bed61a6f62d3e
V L Y <89966395+v-l-y@users.noreply.github.com>
2026-09-24 22:45:31 +0530

    Wire restaurant demo data into local seeding

--- booking/database/seeders/DatabaseSeeder.php
+++ booking/database/seeders/DatabaseSeeder.php
@@ -9,5 +9,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(HotelSetupSeeder::class);
+
+        if (! app()->environment('production')) {
+            $this->call(DemoRestaurantSeeder::class);
+        }
     }
 }
