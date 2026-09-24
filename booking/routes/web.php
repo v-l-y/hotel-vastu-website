3b1909f7b0a09cba82651c4ea9ce130e20dd24db
V L Y <89966395+v-l-y@users.noreply.github.com>
2026-09-24 21:52:29 +0530

    Route front desk discounts

--- booking/routes/web.php
+++ booking/routes/web.php
@@ -87,6 +87,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
             Route::get('/front-desk', [FrontDeskController::class, 'index'])->name('front-desk');
             Route::post('/front-desk/reservations', [FrontDeskController::class, 'createReservation'])->name('front-desk.create');
             Route::post('/front-desk/reservations/{reservation}/modify', [FrontDeskController::class, 'modifyReservation'])->name('front-desk.modify');
+            Route::post('/front-desk/reservations/{reservation}/discount', [FrontDeskController::class, 'applyDiscount'])->name('front-desk.discount');
             Route::post('/front-desk/reservations/{reservation}/check-in', [FrontDeskController::class, 'checkIn'])->name('front-desk.check-in');
             Route::post('/front-desk/reservations/{reservation}/cancel', [FrontDeskController::class, 'cancel'])->name('front-desk.cancel');
             Route::post('/front-desk/reservations/{reservation}/no-show', [FrontDeskController::class, 'noShow'])->name('front-desk.no-show');
